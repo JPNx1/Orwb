d0b9fcbe43e03659ed932fbdca98fe9292179c57
SuicidePandax3 <zimmermann.marlon.mzi@gmail.com>
2019-01-24 14:32:49 +0100

    Control stuff: Jump and gravity implemented

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,6 @@
 let orwb;
-let gravity = 5;
+let gravity = 2.3;
+
 
 //setup function
 function setup() {
@@ -15,35 +16,38 @@ function setup() {
 function draw() {
     background(0);
     orwb.show1();
-    move();
     orwb.applyGravity();
+    move();
 
+    if (orwb.isJumping) {
+        orwb.yVelocity += gravity;
+        orwb.y += orwb.yVelocity;
+
+        if (orwb.y > orwb.ground) {
+            orwb.y = orwb.ground;
+            orwb.yVelocity = 0;
+            orwb.isJumping = false;
+        }
+    }
 }
 
 function move() {
-    if(keyIsDown(LEFT_ARROW)){
-        if(orwb.x >= 36){
+    if (keyIsDown(LEFT_ARROW)) {
+        if (orwb.x >= 36) {
             orwb.goleft();
         }
-
     }
-    if(keyIsDown(RIGHT_ARROW)){
-        if(orwb.x <= window.width - 35){
+    if (keyIsDown(RIGHT_ARROW)) {
+        if (orwb.x <= window.width - 35) {
             orwb.goright();
         }
-
     }
-    if(keyIsDown(32)){
-        orwb.jump();
-    }
-    
 }
 
 function keyPressed() {
-    if(keyCode === 32 ){
-        //orwb.jump();
+    if (keyCode === 32 && !orwb.isJumping) {
+        orwb.jump();
     }
-    
 }
 
 
@@ -55,29 +59,35 @@ class Orwb {
         this.yOffset = 32;
         this.y = this.y - this.yOffset;
         this.x = this.x + this.xOffset;
-        this.speed = 0;
+        this.isJumping = false;
+        this.yVelocity = 0;
+        this.ground = 0;
     }
 
-    applyGravity(){
-        if(this.y <= window.height - 35){
+    applyGravity() {
+        if (this.y <= window.height - 35) {
             this.y = this.y + gravity;
         }
-
     }
 
     goleft() {
-        this.x -= 5;
+        this.x -= 7;
         print("Orwb moved left" + this.x);
     }
 
     goright() {
-        this.x += 5;
+        this.x += 7;
         print("Orwb moved right" + this.x);
     }
 
     jump() {
+        this.ground = this.y;
         print("jump");
-        this.y = this.y - gravity;
+
+        if (this.isJumping === false) {
+            this.yVelocity = -28;
+            this.isJumping = true;
+        }
     }
 
     show1() {
@@ -93,7 +103,5 @@ class Orwb {
 
         ellipse(this.x - 5, this.y - 15, 5, 5);
         ellipse(this.x + 15, this.y - 15, 5, 5);
-
-
     }
 }
