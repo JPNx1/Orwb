e8b5b006b12417490a912d989f5115c1a58d3162
SuicidePandax3 <zimmermann.marlon.mzi@gmail.com>
2019-01-24 15:28:29 +0100

    added classes for game and screens and a switch to determine game state in setup

--- src/js/game.js
+++ src/js/game.js
@@ -1,23 +1,59 @@
+let game;
 let orwb;
+let startScreen;
+let pauseScreen;
+let gameOverScreen;
+let endScreen;
 let gravity = 2.3;
 
 
 //setup function
 function setup() {
+    //creates canvas as object
     let canvas = createCanvas(704, 704);
-    //canvas.parent('p5');
+
+    //framerate
+    frameRate(60);
+
+    //relocates canvas to the p5 html div
     canvas.parent('p5');
+
+    //sets background black
     background(color(0, 0, 0));
+
+    //creates screen objects
+    game = new Game();
+    startScreen = new StartScreen();
+    pauseScreen = new PauseScreen();
+    gameOverScreen = new GameOverScreen();
+    endScreen = new EndScreen();
+
+    //creates an object orwb
     orwb = new Orwb();
-    print(orwb.x, orwb.y);
+
+    //check which state the game has
+    switch (game.state) {
+        case 0:
+
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+    }
 }
 
-//draw function
+//draw function (repeated)
 function draw() {
     background(0);
     orwb.show1();
     orwb.applyGravity();
-    move();
+    orwb.move();
 
     if (orwb.isJumping) {
         orwb.yVelocity += gravity;
@@ -31,19 +67,10 @@ function draw() {
     }
 }
 
-function move() {
-    if (keyIsDown(LEFT_ARROW)) {
-        if (orwb.x >= 36) {
-            orwb.goleft();
-        }
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        if (orwb.x <= window.width - 35) {
-            orwb.goright();
-        }
-    }
-}
+//
 
+
+//checks if a key is pressed
 function keyPressed() {
     if (keyCode === 32 && !orwb.isJumping) {
         orwb.jump();
@@ -51,35 +78,82 @@ function keyPressed() {
 }
 
 
-class Orwb {
+class Game {
     constructor() {
+        this.state = 0;
+    }
+
+    start(){
+
+    }
+
+    pause(){
+
+    }
+
+    resume(){
+
+    }
+
+    end(){
+
+    }
+}
+
+class StartScreen {
+
+}
+
+class PauseScreen {
+
+}
+
+class GameOverScreen {
+
+}
+
+class EndScreen {
+
+}
+
+//blueprint for the object ORWB(player character)
+class Orwb {
+    //construction variables
+    constructor() {
+        //position variables
         this.x = 0;
         this.y = 704;
         this.xOffset = 32;
         this.yOffset = 32;
         this.y = this.y - this.yOffset;
         this.x = this.x + this.xOffset;
+
+        //variables for jumping
         this.isJumping = false;
         this.yVelocity = 0;
         this.ground = 0;
     }
 
+    //applies gravity to orwb
     applyGravity() {
         if (this.y <= window.height - 35) {
             this.y = this.y + gravity;
         }
     }
 
+    //function to change position to the left
     goleft() {
         this.x -= 7;
         print("Orwb moved left" + this.x);
     }
 
+    //function to change position to the right
     goright() {
         this.x += 7;
         print("Orwb moved right" + this.x);
     }
 
+    //function to make orwb jump
     jump() {
         this.ground = this.y;
         print("jump");
@@ -90,18 +164,33 @@ class Orwb {
         }
     }
 
+    //draws orwb at his relative position x and y
     show1() {
+        //yellow base circle for body
         fill(color(255, 212, 0));
         ellipse(this.x, this.y, 64, 64);
 
+        //black circles for eyes
         fill(color(0, 0, 0));
-
         ellipse(this.x - 11, this.y - 10, 20, 20);
         ellipse(this.x + 11, this.y - 10, 20, 20);
 
+        //white circles for reflection in eyes
         fill(color(255, 255, 255));
-
         ellipse(this.x - 5, this.y - 15, 5, 5);
         ellipse(this.x + 15, this.y - 15, 5, 5);
+    }
+
+    move() {
+        if (keyIsDown(LEFT_ARROW)) {
+            if (orwb.x >= 36) {
+                orwb.goleft();
+            }
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            if (orwb.x <= window.width - 35) {
+                orwb.goright();
+            }
+        }
     }
 }
